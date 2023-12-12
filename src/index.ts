ce26854d9973d2fa9008b2a54af5f5c9a525f5ae
Pacifist <desmondmorales92@gmail.com>
2023-12-12 21:46:03 +0200

    adding methods calculate area, print

--- src/index.ts
+++ src/index.ts
@@ -2,75 +2,195 @@
 // У кожної фігури є загальнодоступні властивості - колір і назва, які не можна змінювати після створення.
 // У Square і Rectangle зі свого боку є ще додатковий метод print, який виводить рядок із формулою розрахунку площі
 
-class CalculateArea {
+class Circle {
     private _name: string
     private _color: string
-    private _formula?: string
+    private _radius: number
+    private _area: number
 
-    protected constructor() {}
-
-    protected get name (): string {
-        return this._name
+    constructor (name: string, color: string, radius: number) {
+        this._name = name
+        this._color = color
+        this._radius = radius
     }
 
-    protected set name (figureName: string) {
-        this._name = figureName
-    }
-
-    protected get color (): string {
-        return this._color
-    }
-
-    protected set color (figureColor: string) {
-        this._color = figureColor
-    }
-
-    protected get formula (): string | void {
-        return this._formula
-    }
-
-    protected set formula (square: string) {
-        this._formula = square
-    }
-
-
-}
-
-class Triangle extends CalculateArea {
-    constructor() {
-        super();
-        super.name = 'triangle'
-        super.color = 'red'
+    calculateArea (): number {
+        this._area = Math.PI * Math.pow(this._radius, 2)
+        return this._area
     }
 }
 
-class Circle extends CalculateArea {
-    constructor() {
-        super();
-        super.name = 'circle'
-        super.color = 'green'
+class Rectangle {
+    private _name: string
+    private _color: string
+    private _length: number
+    private _width: number
+    private _area: number
+
+    constructor (name: string, color: string, length: number, width: number) {
+        this._name = name
+        this._color = color
+        this._length = length
+        this._width = width
+    }
+
+    calculateArea (): number {
+        this._area = this._length * this._width
+        return this._area
+    }
+
+    print (): string {
+        return `S = length * width = ${this._length} * ${this._width}`
     }
 }
 
-class Rectangle extends CalculateArea {
-    constructor() {
-        super()
-        super.name = 'rectangle'
-        super.color = 'blue'
-        super.formula = 'l * w'
+class Square {
+    private _name: string
+    private _color: string
+    private _side: number
+    private _area: number
+
+    constructor (name: string, color: string, length: number, side: number) {
+        this._name = name
+        this._color = color
+        this._side
+    }
+
+    calculateArea (): number {
+        this._area = Math.pow(this._side, 2)
+        return this._area
+    }
+
+    print (): string {
+        return `S = side^2 = ${Math.pow(this._side, 2)}`
     }
 }
 
-class Square extends CalculateArea {
-    constructor() {
-        super()
-        super.name = 'square'
-        super.color = 'orange'
-        super.formula = 'a^2'
+class Triangle {
+    private _name: string
+    private _color: string
+    private _base: number
+    private _height: number
+    private _area: number
+
+    constructor (name: string, color: string, base: number, height: number) {
+        this._name = name
+        this._color = color
+        this._base = base
+        this._height = height
+    }
+
+    calculateArea (): number {
+        this._area = (this._height * this._base) / 2
+        return this._area
     }
 }
 
-const triangle = new Triangle()
-const circle = new Circle()
-const rectangle = new Rectangle()
-const square = new Square()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class CalculateArea {
+//     private _name: string
+//     private _color: string
+//     private _formula?: string
+//
+//     protected constructor() {}
+//
+//     protected get name (): string {
+//         return this._name
+//     }
+//
+//     protected set name (figureName: string) {
+//         this._name = figureName
+//     }
+//
+//     protected get color (): string {
+//         return this._color
+//     }
+//
+//     protected set color (figureColor: string) {
+//         this._color = figureColor
+//     }
+//
+//     protected get formula (): string | void {
+//         return this._formula
+//     }
+//
+//     protected set formula (square: string) {
+//         this._formula = square
+//     }
+//
+//
+// }
+//
+// class Print extends CalculateArea{
+//
+// }
+//
+// class Triangle extends CalculateArea {
+//     constructor() {
+//         super();
+//         super.name = 'triangle'
+//         super.color = 'red'
+//     }
+// }
+//
+// class Circle extends CalculateArea {
+//     constructor() {
+//         super();
+//         super.name = 'circle'
+//         super.color = 'green'
+//     }
+// }
+//
+// class Rectangle extends CalculateArea implements Print{
+//     constructor() {
+//         super()
+//         super.name = 'rectangle'
+//         super.color = 'blue'
+//         super.formula = 'l * w'
+//     }
+// }
+//
+// class Square extends CalculateArea {
+//     constructor() {
+//         super()
+//         super.name = 'square'
+//         super.color = 'orange'
+//         super.formula = 'a^2'
+//     }
+// }
+//
+// const triangle = new Triangle()
+// const circle = new Circle()
+// const rectangle = new Rectangle()
+// const square = new Square()
