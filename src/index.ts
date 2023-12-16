f844924c3b6e7ded1e76c0cb22cb2994a3f0af8c
Pacifist <desmondmorales92@gmail.com>
2023-12-16 21:39:22 +0200

    parent class

--- src/index.ts
+++ src/index.ts
@@ -2,43 +2,49 @@
 // У кожної фігури є загальнодоступні властивості - колір і назва, які не можна змінювати після створення.
 // У Square і Rectangle зі свого боку є ще додатковий метод print, який виводить рядок із формулою розрахунку площі
 
+class Shape {
+    private _color: string
+    private _name: string
+
+    constructor(name: string, color: string) {
+        this._name = name
+        this._color = color
+    }
+
+    calculateArea(): number {return 0}
+}
+
 interface IPrint {
     print(): string
 }
 
-class Circle {
-    private _name: string
-    private _color: string
+class Circle extends Shape {
     private _radius: number
     private _area: number
 
     constructor (name: string, color: string, radius: number) {
-        this._name = name
-        this._color = color
+        super(name, color)
         this._radius = radius
     }
 
-    calculateArea (): number {
+    override calculateArea (): number {
         this._area = Math.PI * Math.pow(this._radius, 2)
         return this._area
     }
 }
 
-class Rectangle implements IPrint {
-    private _name: string
-    private _color: string
+class Rectangle extends Shape implements IPrint {
     private _length: number
     private _width: number
     private _area: number
 
     constructor (name: string, color: string, length: number, width: number) {
-        this._name = name
-        this._color = color
+        super(name, color)
         this._length = length
         this._width = width
     }
 
-    calculateArea (): number {
+    override calculateArea (): number {
         this._area = this._length * this._width
         return this._area
     }
@@ -48,19 +54,16 @@ class Rectangle implements IPrint {
     }
 }
 
-class Square implements IPrint {
-    private _name: string
-    private _color: string
+class Square extends Shape implements IPrint {
     private _side: number
     private _area: number
 
-    constructor (name: string, color: string, length: number, side: number) {
-        this._name = name
-        this._color = color
-        this._side
+    constructor (name: string, color: string, side: number) {
+        super(name, color)
+        this._side = side
     }
 
-    calculateArea (): number {
+    override calculateArea (): number {
         this._area = Math.pow(this._side, 2)
         return this._area
     }
@@ -70,131 +73,19 @@ class Square implements IPrint {
     }
 }
 
-class Triangle {
-    private _name: string
-    private _color: string
+class Triangle extends Shape {
     private _base: number
     private _height: number
     private _area: number
 
     constructor (name: string, color: string, base: number, height: number) {
-        this._name = name
-        this._color = color
+        super(name, color)
         this._base = base
         this._height = height
     }
 
-    calculateArea (): number {
+    override calculateArea (): number {
         this._area = (this._height * this._base) / 2
         return this._area
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class CalculateArea {
-//     private _name: string
-//     private _color: string
-//     private _formula?: string
-//
-//     protected constructor() {}
-//
-//     protected get name (): string {
-//         return this._name
-//     }
-//
-//     protected set name (figureName: string) {
-//         this._name = figureName
-//     }
-//
-//     protected get color (): string {
-//         return this._color
-//     }
-//
-//     protected set color (figureColor: string) {
-//         this._color = figureColor
-//     }
-//
-//     protected get formula (): string | void {
-//         return this._formula
-//     }
-//
-//     protected set formula (square: string) {
-//         this._formula = square
-//     }
-//
-//
-// }
-//
-// class Print extends CalculateArea{
-//
-// }
-//
-// class Triangle extends CalculateArea {
-//     constructor() {
-//         super();
-//         super.name = 'triangle'
-//         super.color = 'red'
-//     }
-// }
-//
-// class Circle extends CalculateArea {
-//     constructor() {
-//         super();
-//         super.name = 'circle'
-//         super.color = 'green'
-//     }
-// }
-//
-// class Rectangle extends CalculateArea implements Print{
-//     constructor() {
-//         super()
-//         super.name = 'rectangle'
-//         super.color = 'blue'
-//         super.formula = 'l * w'
-//     }
-// }
-//
-// class Square extends CalculateArea {
-//     constructor() {
-//         super()
-//         super.name = 'square'
-//         super.color = 'orange'
-//         super.formula = 'a^2'
-//     }
-// }
-//
-// const triangle = new Triangle()
-// const circle = new Circle()
-// const rectangle = new Rectangle()
-// const square = new Square()
